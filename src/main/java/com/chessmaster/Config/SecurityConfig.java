3ab5f170606336365bdadd468f5774dcc0ab160c
punit1808 <112548386+punit1808@users.noreply.github.com>
2025-06-14 18:47:39 +0530

    Update SecurityConfig.java

--- src/main/java/com/chessmaster/Config/SecurityConfig.java
+++ src/main/java/com/chessmaster/Config/SecurityConfig.java
@@ -1,21 +1,33 @@
 package com.chessmaster.Config;
+
 import com.chessmaster.jwt.JwtAuthFilter;
 
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.ResponseCookie;
+
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.Customizer;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
 
 @Configuration
 @EnableWebSecurity
