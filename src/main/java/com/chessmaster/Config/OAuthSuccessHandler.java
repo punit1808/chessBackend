579b3caa96f1251db690cbcd1582d238d5aa6ad4
punit1808 <punityadav1808@gmail.com>
2025-06-19 12:16:43 +0530

    update

--- src/main/java/com/chessmaster/Config/OAuthSuccessHandler.java
+++ src/main/java/com/chessmaster/Config/OAuthSuccessHandler.java
@@ -1,15 +1,14 @@
 package com.chessmaster.Config;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.ServletException;
-import jakarta.persistence.*;
+import java.io.IOException;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class OAuthSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
