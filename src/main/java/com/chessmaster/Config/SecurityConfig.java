1cca0ea879d81c0a40e3faa56f3d10de4d3ef234
punit1808 <punityadav1808@gmail.com>
2025-06-19 13:56:13 +0530

    cors added

---
+++ src/main/java/com/chessmaster/Config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.chessmaster.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                            "https://chess-frontend-ashy.vercel.app",
+                            "https://chess-frontend-punit1808s-projects.vercel.app",
+                            "https://chess-frontend-git-main-punit1808s-projects.vercel.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true)
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
+
